715679225601bebcd07662b3e432700819f3600a
Kylian Bardini <kylianb1@icloud.com>
2023-04-07 12:31:22 +0200

    🧪 thunder client setup clients routes

--- bill-backend-app/controlllers/clients.controller.js
+++ bill-backend-app/controlllers/clients.controller.js
@@ -1,27 +1,27 @@
 const Client = require('../models/clients.model.js');
 
 // récupérer la liste, correspond à la route GET /clients
-exports.getClients = (req, res) => {
+const getClients = (req, res) => {
     res.json('getClients')
 }
 
 // récupérer un élément unique, correspond à la route GET /clients/:id
-exports.getClient = (req, res) => {
+const getClient = (req, res) => {
     res.json('getClient')
 }
 
 // modifie un élément unique, correspond à la route PATCH /clients/:id
-exports.patchClient = (req, res) => {
+const patchClient = (req, res) => {
     res.json('patchClient')
 }
 
 // Crée un élément, correspond à la route POST /clients/
-exports.postClient = (req, res) => {
+const postClient = (req, res) => {
     res.json('postClient')
 }
 
 // supprime un élément unique, correspond à la route DELETE /clients/:id
-exports.deleteClient = (req, res) => {
+const deleteClient = (req, res) => {
     res.json('deleteClient')
 }
 
